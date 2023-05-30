f86f614e6725a7f7a51fede92c5d515f52da9d11
shotam1415 <shotam1415@gmail.com>
2023-05-30 18:54:21 +0900

    [WIP]環境構築を終えて、テスト用のコンポーネントを作成途中

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+type StateType = {
+  currentCount: number;
+};
 
 export default new Vuex.Store({
   state: {
+    currentCount: 0,
   },
-  getters: {
-  },
+  getters: { currentCount: (state: StateType): number => state.currentCount },
   mutations: {
+    increment: (state: StateType): void => {
+      state.currentCount++;
+    },
+    decrement: (state: StateType): void => {
+      state.currentCount--;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
---
+++ src/views/TestView.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card class="mx-auto" max-width="344" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">TEST-STUDY</div>
+        <v-list-item-title class="text-h5 mb-1">
+          COUNT-CARD 0</v-list-item-title
+        >
+        <v-list-item-subtitle
+          >ボタンを押すとタイトルの数字が変化します。</v-list-item-subtitle
+        >
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn outlined rounded x-small>-</v-btn>
+      <v-btn outlined rounded x-small>+</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class TestView extends Vue {}
+</script>
